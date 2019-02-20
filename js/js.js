ff271bf118fe1b709ab66e827b5fcb0c78b22b35
User <irshin.tima@mail.ru>
2019-02-20 13:14:07 +0700

    button subscription

--- js/js.js
+++ js/js.js
@@ -1,29 +1,62 @@
 
 // url (required), options (optional)
-fetch( 'table.json', {
+/*fetch( 'table.json', {
     method: 'get'
 }).then( function( response ) {
     console.log(response.json());
     // Success
-}).catch( function( err ) {
+}).catch( function( err ) {*/
     // Error
-});
+(function($) {
+    "use strict"; // Start of use strict
+    function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+               return re.test(email);
+    }
+    var encoded = "&bull;&nbsp;";
+    var bull = $("<div/>").html(encoded).text();
+    // Select all links with hashes
 
-function infoalert() {
-    alert("Запуск сайта будет произведен в ближайшее время");
-}
-$("#platform").click(function() {
-    infoalert()
-});
-$("#news").click(function() {
-    infoalert()
+    //Collect email
+    $("#emailSubmit").click(function () {
+        if (validateEmail($("#userEmail").val()) === true ) {
+            try {
+                gaSendEvent('Email Subscription', $("#userEmail").val());
+                FS.setUserVars({
+                    "displayName": $("#userEmail").val(),
+                    "email": $("#userEmail").val()
+                });
+            } catch (e) {
+            }
+            alert("Спасибо за подписку на новости: " + $("#userEmail").val().toString() + "!");
+            $("#userEmail").val("");
+        } else {
+            var infoText = "";
+            if (validateEmail($("#userEmail").val()) === false) {
+                infoText += bull + "Email указан не верно\n";
+            }
+            alert(infoText);
+        }
+        function infoalerto() {
+            alert("Запуск сайта будет произведен в ближайшее время");
+        }
+        $("#fourm").click(function()
+            {infoalerto()
+
+    $("#news").click(function() {
+    infoalerto()
 });
 $("#node").click(function() {
-    infoalert()
+    infoalerto()
 });
 $("#contact").click(function() {
-    infoalert()
+    infoalerto()
+
 });
+            });
+                });
+
+})(jQuery); // End of use strict
 /*
 var collection = '{"tr": [{"td1": "name1","td2": "name2"},{"td1": "name1","td2": "name2"}]}';
 var tr += '';
